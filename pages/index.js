a9722e62d727c6229b5b1abadad4fb9ea8d8ae70
Natanael Pacheco <natepacheco716@gmail.com>
2022-03-21 10:36:22 -0500

    Initilized Next.js, added design assets.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>QuickSurf - Same Day Surf Board Delivery</title>
+        <title>QuickSurf - Custom SurfBoards Same Day Delivery</title>
         <meta name="description" content="Same Day Delivery Custom Surf Boards" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hompage
+      homepage
+        <FontAwesomeIcon icon={faCoffee} />
     </div>
   )
 }
+
+
