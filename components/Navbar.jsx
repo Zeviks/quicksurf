2980481901ce3670d789b16fbdcde67715caee2f
Natanael Pacheco <natepacheco716@gmail.com>
2022-03-22 07:02:53 -0500

    Removed Unused Styles.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
       </div>
 
       <div className={styles.item}></div>
-
       <div className={styles.item}></div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Home() {
@@ -15,7 +13,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       homepage
-        <FontAwesomeIcon icon={faCoffee} />
     </div>
   )
 }
