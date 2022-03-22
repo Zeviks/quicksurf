41bbe38b2ec0c5a89e7a0477e31fbaa51d76f938
Natanael Pacheco <natepacheco716@gmail.com>
2022-03-22 07:00:53 -0500

    Integrated Font Awesome.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,15 @@
 import styles from "../styles/Navbar.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-        </div>
-      
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>      
       </div>
 
       <div className={styles.item}></div>
