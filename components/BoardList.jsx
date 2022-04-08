e38132823e62aacd8de9697f322c58309e871733
Natanael Pacheco <natepacheco716@gmail.com>
2022-04-08 08:42:10 -0500

    Completed Product Section.

--- components/BoardList.jsx
+++ components/BoardList.jsx
@@ -2,25 +2,23 @@ import BoardCard from './BoardCard';
 import styles from '../styles/BoardList.module.css';
 import Image from 'next/image';
 
-
 const BoardList = () => {
+  
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Our Award Winning Boards</h1>
+      <h1 className={styles.title}>Award Winning Surf Boards</h1>
       <p className={styles.desc}>Our surfboards have recieved some of the most coveted awards in the industry.
-      We are proud to be the best in the game. Our surfboards are made completly in the USA. We manufactured everything in
-      house in order to produce the quality of product we are globaly known for. 
-      Our products are our identity, and our customers are our family. 
+      We are proud to be the best in the game. Our surfboards are completly manufactured in the United States of America. We Guarantee Satisfaction.
       </p>
       <div className={styles.wrapper}>
-          <BoardCard />
-          <BoardCard />
-          <BoardCard />
-          <BoardCard />
-          <BoardCard />
-          <BoardCard />
-          <BoardCard />
-          <BoardCard />
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"100"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"150"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"200"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"250"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"300"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"350"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"400"}/>
+          <BoardCard aosDir={"zoom-in-up"} aosDelay={"450"}/>
       </div>
     </div>
   )
