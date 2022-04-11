b195008ada09a084e2b2da1a6115ca2a5a61ea5f
Natanael Pacheco <natepacheco716@gmail.com>
2022-04-11 03:44:23 -0500

    Created footer component, added footer elements.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,45 @@
 import styles from '../styles/Footer.module.css'
 const Footer = () => {
   return (
-    <div className={styles.container}>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>LOGO</div>
+     <div> 
+      <h2>FIND OUR STORES</h2>
+        <div>
+          <h2>NEW YORK</h2>
+           <p>1654 R. Don Road #304</p>
+           <p>NewYork, 85022</p>
+           <p>(602)-807-1010</p>
+        </div>
+        <div>
+          <h2>TEXAS</h2>
+           <p>1654 R. Don Road #304</p>
+           <p>NewYork, 85022</p>
+           <p>(602)-807-1010</p>
+        </div>
+        <div>
+          <h2>FLORIDA</h2>
+           <p>1654 R. Don Road #304</p>
+           <p>NewYork, 85022</p>
+           <p>(602)-807-1010</p>
+        </div>
+      </div>
+     <div> 
+      <h2>WORKING HOURS</h2>
+        <div>
+          <h2>MONDAY - FRIDAY</h2>
+           <p>1654 R. Don Road #304</p>
+           <p>NewYork, 85022</p>
+           <p>(602)-807-1010</p>
+        </div>
+        <div>
+          <h2></h2>
+           <p>1654 R. Don Road #304</p>
+           <p>NewYork, 85022</p>
+           <p>(602)-807-1010</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
